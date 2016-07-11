25d62022fb34653c22e0fbcebe78631408dbc30e
ivanhu42 <ivanhu42@gmail.com>
2016-07-11 20:02:08 +0800

    adjust indentation

--- codeforces/cpp/680b.cpp
+++ codeforces/cpp/680b.cpp
@@ -16,22 +16,22 @@ typedef long long ll;
 
 int main ()
 {
-        int n, a, i, t, r = 0;
-        cin >> n >> a;
-        vector<int> v;
-        for (i = 0; i < n; i++)
-        {
-                cin >> t;
-                r += t;
-                v.push_back(t);
-        }
-        int l = min(n - a, a - 1);
-        for (i = 1; i < 1 + l; i++)
-        {
-                if (v[a - 1 + i] != v[a - 1 - i])
-                        r--;
-        }
-        cout << r << endl;
-        return 0;
+  int n, a, i, t, r = 0;
+  cin >> n >> a;
+  vector<int> v;
+  for (i = 0; i < n; i++)
+  {
+    cin >> t;
+    r += t;
+    v.push_back(t);
+  }
+  int l = min(n - a, a - 1);
+  for (i = 1; i < 1 + l; i++)
+  {
+    if (v[a - 1 + i] != v[a - 1 - i])
+    r--;
+  }
+  cout << r << endl;
+  return 0;
 }
 
